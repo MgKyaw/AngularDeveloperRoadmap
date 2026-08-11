10bcbe9a07a483deee9d8031f8f0f452b3c20298
MgKyaw <mgkyaw20112@gmail.com>
2026-08-11 21:45:58 +0700

    feat: Update AppComponent.ts

--- todo/src/AppComponent.ts
+++ todo/src/AppComponent.ts
@@ -13,3 +13,16 @@
 // export class AppComponent {
 //   // code goes here
 // }
+
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [],
+  template: "<h1>{{ title }}</h1>",
+  styleUrl: "./app.component.css",
+})
+export class AppComponent {
+  title = "To do application";
+}
