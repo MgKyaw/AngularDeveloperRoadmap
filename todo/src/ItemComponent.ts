2688599866f4005d25f97a55a33af379b8d2536f
MgKyaw <mgkyaw20112@gmail.com>
2026-08-11 21:43:03 +0700

    feat: Add ItemComponent.ts

---
+++ todo/src/ItemComponent.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-item",
+  standalone: true,
+  imports: [],
+  // the following metadata specifies the location of the other parts of the component
+  templateUrl: "./item.component.html",
+  styleUrl: "./item.component.css",
+})
+export class ItemComponent {
+  // code goes here
+}
