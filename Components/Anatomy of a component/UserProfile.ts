9a01e323cba3a48bf0e8addf113f65295a636d42
MgKyaw <mgkyaw20112@gmail.com>
2026-08-14 23:35:30 +0700

    feat: Update UserProfile.ts

--- Components/Anatomy of a component/UserProfile.ts
+++ Components/Anatomy of a component/UserProfile.ts
@@ -6,3 +6,13 @@
 //   /* ... */
 // })
 // export class UserProfile {}
+
+// @Component({
+//   selector: 'profile-photo',
+// })
+// export class ProfilePhoto {}
+// @Component({
+//   imports: [ProfilePhoto],
+//   template: `<profile-photo />`,
+// })
+// export class UserProfile {}
