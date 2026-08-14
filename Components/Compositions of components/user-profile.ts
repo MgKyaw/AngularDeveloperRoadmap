993135dcb4ab0c7064621fbefa4b3efc6c66796e
MgKyaw <mgkyaw20112@gmail.com>
2026-08-14 23:37:48 +0700

    feat: Add user-profile.ts

--- Components/Compositions of components/user-profile.ts
+++ Components/Compositions of components/user-profile.ts
@@ -26,3 +26,18 @@
 // export class UserProfile {
 //   /* Your component code goes here */
 // }
+
+// // user-profile.ts
+// import {ProfilePhoto} from 'profile-photo.ts';
+// @Component({
+//   selector: 'user-profile',
+//   imports: [ProfilePhoto],
+//   template: `
+//     <h1>User profile</h1>
+//     <profile-photo />
+//     <p>This is the user profile page</p>
+//   `,
+// })
+// export class UserProfile {
+//   // Component behavior is defined in here
+// }
