b7288c3d45477b1e5e45d9ab74366df169e597cb
MgKyaw <mgkyaw20112@gmail.com>
2026-08-16 22:05:50 +0700

    feat: Add answer

--- Components/Component Interactive Tutorials/Tutorial3.ts
+++ Components/Component Interactive Tutorials/Tutorial3.ts
@@ -14,3 +14,21 @@
 //   imports: [],
 // })
 // export class App {}
+
+// import {Component} from '@angular/core';
+
+// @Component({
+//   selector: 'app-user',
+//   template: ` Username: {{ username }} `,
+// })
+// export class User {
+//   username = 'youngTech';
+// }
+
+// @Component({
+//   selector: 'app-root',
+//   template: `<app-user />`,
+//   imports: [User]
+// })
+// export class App {}
+
