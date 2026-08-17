2229c2e050de82c54c0bd51879be3b9c6b3e451f
MgKyaw <mgkyaw20112@gmail.com>
2026-08-17 22:25:57 +0700

    feat: Add users array

--- Components/Component Interactive Tutorials/ForControlFlow.ts
+++ Components/Component Interactive Tutorials/ForControlFlow.ts
@@ -4,4 +4,12 @@
 //   selector: 'app-root',
 //   template: ``,
 // })
-// export class App {}
+// export class App {
+    // users = [
+    // {id: 0, name: 'Sarah'},
+    // {id: 1, name: 'Amy'},
+    // {id: 2, name: 'Rachel'},
+    // {id: 3, name: 'Jessica'},
+    // {id: 4, name: 'Poornima'},
+    // ];
+// }
