16d14ec06a1dcc4e0309bf9d82f187efca225352
MgKyaw <mgkyaw20112@gmail.com>
2026-08-21 21:41:13 +0700

    feat: Add a minimum duration

--- Components/Component Interactive Tutorials/DeferableViews/app.ts
+++ Components/Component Interactive Tutorials/DeferableViews/app.ts
@@ -11,7 +11,7 @@
         //     <comments />
         // } @placeholder {
     //     <p>Future comments</p>
-    //   } @loading {
+    //   }  @loading (minimum 2s) {
 //   <p>Loading comments...</p>
 // }
 //     </div>
