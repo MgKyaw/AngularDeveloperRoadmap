bfd98942240bfd0116d77a9ad39f85e1b0b3b07f
MgKyaw <mgkyaw20112@gmail.com>
2026-08-28 22:13:36 +0700

    feat: Add form

--- Components/Components/src/app/Reactive Forms/app.ts
+++ Components/Components/src/app/Reactive Forms/app.ts
@@ -4,17 +4,17 @@ import {ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   template: `
-    <form>
-      <label>
-        Name
-        <input type="text" formControlName="name" />
-      </label>
-      <label>
-        Email
-        <input type="email" formControlName="email" />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
+    <form [formGroup]="profileForm">
+  <label>
+    Name
+    <input type="text" formControlName="name" />
+  </label>
+  <label>
+    Email
+    <input type="email" formControlName="email" />
+  </label>
+  <button type="submit">Submit</button>
+</form>
   `,
   imports: [ReactiveFormsModule],
 })
