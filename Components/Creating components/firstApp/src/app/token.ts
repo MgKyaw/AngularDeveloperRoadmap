2a4f03348a388294657be61d15d1d26619ccae50
MgKyaw <mgkyaw20112@gmail.com>
2026-09-06 10:57:24 +0700

    feat: Add token.ts

---
+++ Components/Creating components/firstApp/src/app/token.ts
@@ -0,0 +1,4 @@
+// // 📁 /app/tokens.ts
+// import {InjectionToken} from '@angular/core';
+// import {DataService} from './data-service.interface';
+// export const DATA_SERVICE_TOKEN = new InjectionToken<DataService>('DataService');
