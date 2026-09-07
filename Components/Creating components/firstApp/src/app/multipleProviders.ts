ea735affc5939acc6248232c616b06ea46c3e0e3
MgKyaw <mgkyaw20112@gmail.com>
2026-09-07 21:29:55 +0700

    feat: Add multiple providers

---
+++ Components/Creating components/firstApp/src/app/multipleProviders.ts
@@ -0,0 +1,6 @@
+// export const INTERCEPTOR_TOKEN = new InjectionToken<Interceptor[]>('interceptors');
+// providers: [
+//   {provide: INTERCEPTOR_TOKEN, useClass: AuthInterceptor, multi: true},
+//   {provide: INTERCEPTOR_TOKEN, useClass: LoggingInterceptor, multi: true},
+//   {provide: INTERCEPTOR_TOKEN, useClass: RetryInterceptor, multi: true},
+// ];
