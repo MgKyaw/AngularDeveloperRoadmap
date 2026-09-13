596713b8047503e84a0dfd978eb7a5f0618ebdbf
MgKyaw <mgkyaw20112@gmail.com>
2026-09-13 21:40:51 +0700

    feat: Add ComponentWithCustomElement.ts

---
+++ Components/ViewProvider/ViewProvider/src/app/componentWithCustomElement.ts
@@ -0,0 +1,7 @@
+// import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+// @Component({
+//   ...,
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA],
+//   template: '<some-unknown-component />'
+// })
+// export class ComponentWithCustomElements { }
