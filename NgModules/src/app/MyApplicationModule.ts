4f1c242873482ece277513faa7aa546ac1a61958
MgKyaw <mgkyaw20112@gmail.com>
2026-09-15 19:12:26 +0700

    feat: Add MyApplicationModule

---
+++ NgModules/src/app/MyApplicationModule.ts
@@ -0,0 +1,6 @@
+// import {platformBrowser} from '@angular/platform-browser';
+// @NgModule({
+//   bootstrap: [MyApplication],
+// })
+// export class MyApplicationModule {}
+// platformBrowser().bootstrapModule(MyApplicationModule);
