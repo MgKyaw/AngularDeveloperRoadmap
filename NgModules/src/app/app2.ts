79f7e36f9324a26e33428eb94de22a8b6af92a35
MgKyaw <mgkyaw20112@gmail.com>
2026-09-16 21:38:57 +0700

    feat: Add app2.ts

--- NgModules/src/app/app2.ts
+++ NgModules/src/app/app2.ts
@@ -7,3 +7,17 @@
 // export class App {
 //   theme = 'dark';
 // }
+
+// @Component({
+//   template: `
+//     <!-- Does not necessarily update when `welcomeMessage` changes. -->
+//     <p>{{ welcomeMessage }}</p>
+
+//     <p>Your color preference is {{ theme() }}.</p> <!-- Always updates when the value of the `theme` signal changes. -->
+//   `
+//   ...
+// })
+// export class App {
+//   welcomeMessage = "Welcome, enjoy this app that we built for you";
+//   theme = signal('dark');
+// }
