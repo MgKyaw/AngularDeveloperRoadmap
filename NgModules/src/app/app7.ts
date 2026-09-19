073718d4e4f4ebd6060108647ea818fe14cbe7df
MgKyaw <mgkyaw20112@gmail.com>
2026-09-19 10:30:00 +0700

    feat: Comment app7.ts

--- NgModules/src/app/app7.ts
+++ NgModules/src/app/app7.ts
@@ -1,9 +1,9 @@
-@Component({
-  template: `
-    <p>Your color preference is {{ theme }}.</p>
-  `,
-  ...
-})
-export class App {
-  theme = 'dark';
-}
+// @Component({
+//   template: `
+//     <p>Your color preference is {{ theme }}.</p>
+//   `,
+//   ...
+// })
+// export class App {
+//   theme = 'dark';
+// }
