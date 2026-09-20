7de74c3f7ac604943f2b029d2d30402e33ad28aa
MgKyaw <mgkyaw20112@gmail.com>
2026-09-20 23:17:33 +0700

    feat: Add app9.ts

---
+++ NgModules/src/app/app9.ts
@@ -0,0 +1,11 @@
+// @Component({
+//   template: `
+//     <input type="text" (keyup.enter)="updateField($event)" />
+//   `,
+//   ...
+// })
+// export class App{
+//   updateField(event: KeyboardEvent): void {
+//     console.log('The user pressed enter in the text field.');
+//   }
+// }
