cc048e61427ee539b1101165c888abdcd1d00022
MgKyaw <mgkyaw20112@gmail.com>
2026-09-21 19:54:32 +0700

    feat: Add listeningOnGlobalTarget

---
+++ NgModules/src/app/listeningOnGlobalTarget.ts
@@ -0,0 +1,9 @@
+// @Component({
+//   /* ... */
+//   host: {
+//     '(window:click)': 'onWindowClick()',
+//     '(document:click)': 'onDocumentClick()',
+//     '(body:click)': 'onBodyClick()',
+//   },
+// })
+// export class MyView {}
