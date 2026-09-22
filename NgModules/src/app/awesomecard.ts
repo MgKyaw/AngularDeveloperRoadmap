90879ae22fd993508aa2067aa94679bffc6f5cae
MgKyaw <mgkyaw20112@gmail.com>
2026-09-22 21:14:44 +0700

    feat: Add awesome card

---
+++ NgModules/src/app/awesomecard.ts
@@ -0,0 +1,11 @@
+// @Component({
+//   ...,
+//   host: {
+//     '(click.debounce.500)': 'handleDebouncedClick()',
+//   },
+// })
+// export class AwesomeCard {
+//   handleDebouncedClick(): void {
+//    console.log('Debounced click!');
+//   }
+// }
