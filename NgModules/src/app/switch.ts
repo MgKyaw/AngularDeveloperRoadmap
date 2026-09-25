80bcc167134435fefeccfb2a617d4aab3f46fca4
MgKyaw <mgkyaw20112@gmail.com>
2026-09-25 21:31:43 +0700

    feat: Add switch.ts

---
+++ NgModules/src/app/switch.ts
@@ -0,0 +1,12 @@
+// @switch (userPermissions) {
+//   @case ('admin') {
+//     <app-admin-dashboard />
+//   }
+//   @case ('reviewer')
+//   @case ('editor') {
+//     <app-editor-dashboard />
+//   }
+//   @default {
+//     <app-viewer-dashboard />
+//   }
+// }
