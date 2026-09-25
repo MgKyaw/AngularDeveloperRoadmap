e156de592cb02e17315af9166ee109825f850b92
MgKyaw <mgkyaw20112@gmail.com>
2026-09-25 21:30:13 +0700

    feat: Add if else

--- NgModules/src/app/ifelse.ts
+++ NgModules/src/app/ifelse.ts
@@ -1,3 +1,11 @@
 // @if (a > b) {
 //   <p>{{ a }} is greater than {{ b }}</p>
 // }
+
+// @if (a > b) {
+//   {{ a }} is greater than {{ b }}
+// } @else if (b > a) {
+//   {{ a }} is less than {{ b }}
+// } @else {
+//   {{ a }} is equal to {{ b }}
+// }
